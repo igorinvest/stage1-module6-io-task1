8a2bed36d3d19468d94448da58a56ee4d19a0348
igorinvest <igorchvalun@gmail.com>
2023-10-01 15:25:59 +0530

    Fixed logger?

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,36 +1,37 @@
 package com.epam.mjc.io;
 
 import java.io.*;
-
+import java.util.logging.Logger;
 
 public class FileReader {
-
+    static Logger log = Logger.getLogger(FileReader.class.getName());
+    
     public Profile getDataFromFile(File file) {
-
-        //InputStream targetStream = Streams.getStreamFromFile(file);
-        //String st = Streams.convertStreamToString(targetStream);
         InputStream targetStream = null;
         try{
             targetStream = new DataInputStream(new FileInputStream(file));
         } catch (FileNotFoundException e) {
-            System.out.println(e);
+            log.info(e.toString());
         }
 
-        byte[] bytes;
-        String st = "";
+        byte[] bytes = new byte[0];
         try {
-            //targetStream = new DataInputStream(new FileInputStream(file));
             bytes = targetStream.readAllBytes();
-            st = new String(bytes, "UTF-8");
-
         } catch (IOException e) {
-            System.out.println(e);
+            log.info(e.toString());
         } finally {
             try{
                 targetStream.close();
             } catch (IOException e) {
-                System.out.println(e);
+                log.info(e.toString());
             }
+        }
+
+        String st = "";
+        try {
+            st = new String(bytes, "UTF-8");
+        } catch (IOException e) {
+            log.info(e.toString());
         }
 
         String name = "";
@@ -50,58 +51,8 @@ public class FileReader {
                 email = keyValue.value;
             } else if (keyValue.key.equals("Phone")) {
                 phone = Long.parseLong(keyValue.value);
-            };
+            }
         }
-        Profile profile = new Profile(name, age, email, phone);
-
-        return profile;
-    }
-}
-
-class KeyValue {
-    String key;
-    String value;
-
-    public KeyValue(String line) {
-        String[] splitted = line.split(": ");
-        this.key = splitted[0];
-        this.value = splitted[1];
-    };
-}
-
-class Streams {
-    public static InputStream getStreamFromFile(File file) {//throws FileNotFoundException
-        InputStream targetStream = null;
-        try{
-            targetStream = new DataInputStream(new FileInputStream(file));
-        } catch (FileNotFoundException e) {
-            System.out.println(e);
-        }
-        //InputStream targetStream = new DataInputStream(new FileInputStream(file));
-        return targetStream;
-    }
-
-    public static void closeStream(InputStream stream) {//throws FileNotFoundException
-        try{
-            stream.close();
-        } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
-
-    public static String convertStreamToString(InputStream stream) {//throws FileNotFoundException
-        byte[] bytes;
-        String st = "";
-        try {
-            //targetStream = new DataInputStream(new FileInputStream(file));
-            bytes = stream.readAllBytes();
-            st = new String(bytes, "UTF-8");
-
-        } catch (IOException e) {
-            System.out.println(e);
-        } finally {
-            Streams.closeStream(stream);
-        }
-        return st;
+        return new Profile(name, age, email, phone);
     }
 }
